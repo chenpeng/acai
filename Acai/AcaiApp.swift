e56e3472b38b6ae9b918c52718cf0a2a9ae218b3
Chen Peng <chenpeng.net@gmail.com>
2021-10-20 14:43:53 +0800

    Initial Commit

---
+++ Acai/AcaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcaiApp.swift
+//  Acai
+//
+//  Created by 陈鹏 on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AcaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
